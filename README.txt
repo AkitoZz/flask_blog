8c5df57f36854adf52645a36a60cd990f84d37a6
Mikazuki <chenzg1996@gmail.com>
2018-07-22 13:48:59 +0800

    添加配置文件

---
+++ README.txt
@@ -0,0 +1,5 @@
+20180722
+该博客系统是根据<flask web开发>这个书为基础进行开发的，原项目地址git@github.com:miguelgrinberg/flasky.git
+
+作为个人博客用权限与用户角色会变得很简单，相当于只有admin和guest两种角色
+
--- manager.py
+++ manager.py
@@ -3,17 +3,27 @@ import os
 from app import create_app,db
 from app.models import User,Role,Permission,Post,Comment
 from flask_script import Manager,Shell
-from flask_migrate import Migrate,MigrateCommand
+from flask_migrate import Migrate,MigrateCommand,upgrade
 
 app = create_app(os.getenv('FLASK_CONFIG') or 'default')
 manager = Manager(app)
 migrate = Migrate(app,db)
 
 def make_shell_context():
-    return dict(app=app,db=db,User=User,Role=Role,Post=Post)
+    return dict(app=app,db=db,User=User,Role=Role,Post=Post,Comment=Comment)
 
 manager.add_command('shell',Shell(make_context=make_shell_context))
 manager.add_command('db',MigrateCommand)
+
+@app.cli.command()
+def deploy():
+    """Run deployment tasks."""
+    # migrate database to latest revision
+    upgrade()
+
+    # create or update user roles
+    Role.insert_roles()
+
 
 if __name__ == '__main__':
     manager.run()
